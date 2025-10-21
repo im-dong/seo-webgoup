73b9ce8120dd805cbfb8d892cf86fcefe51532a1
dong <dong@test.com>
2025-10-22 00:55:47 +0800

    debug: 添加phpinfo()检查服务器PHP版本兼容性

--- app/controllers/TestEmail.php
+++ app/controllers/TestEmail.php
@@ -7,6 +7,10 @@ class TestEmail extends Controller {
     }
 
     public function index() {
+        // 添加PHP信息检查
+        phpinfo();
+        die();
+
         // 检查是否是管理员，或者简单允许所有用户访问测试页面
         // 如果需要限制访问，可以在这里添加权限检查
 
